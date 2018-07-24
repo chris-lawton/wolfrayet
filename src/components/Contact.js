ee2fec0c8fcdcd74abb0cb858e0ebd7fae4d5076
Chris Lawton <chris.lawton@torchbox.com>
2018-07-24 21:32:19 +0100

    correct class names

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class About extends Component {
+class Contact extends Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ class About extends Component {
   }
 }
 
-export default About;
+export default Contact;
