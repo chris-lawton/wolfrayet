5f5b474f5fccf40eae061f93b59b2ee160368f77
Chris Lawton <chris.lawton@torchbox.com>
2018-07-26 20:10:11 +0100

    stockists page and responsive nav styles

--- src/components/Stockists.js
+++ src/components/Stockists.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Image from './../images/etsy.png'
 
 class Stockists extends Component {
   render() {
     return (
-      <div>
-          Stockists
+      <div className="container container--narrow stockists">
+        <div>
+          <p>
+            Kim West 47,<br />
+            Denmark Hill,<br />
+            London,<br />
+            SE5 8RS
+          </p>
+
+          <p>Tues-Sat 11-7</p>
+
+          <p>Tel: 020 7703 6949</p>
+        </div>
+
+        <div>
+          <p>Discover Wolf Rayet on</p>
+          <a target="_blank" href="https://www.etsy.com/uk/shop/WolfRayetLondon">
+            <img className="stockists__image" src={Image} alt="Etsy"/>
+          </a>
+        </div>
+
       </div>
     );
   }
