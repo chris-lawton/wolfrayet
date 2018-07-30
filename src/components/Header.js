4e285297d1b7d19c39d9ebee3af6646757e9ed78
Chris Lawton <chris.lawton@torchbox.com>
2018-07-30 21:07:53 +0100

    remove everything unnecessary from the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,96 +1,12 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import Cart from './Cart';
 import Image from './../images/wolf-rayet-logo.jpg'
 
 class Header extends Component {
-  constructor() {
-      super();
-
-      this.state = {
-        isCartOpen: false,
-        checkout: { lineItems: [] },
-        products: [],
-        shop: {}
-      };
-
-      this.handleCartClose = this.handleCartClose.bind(this);
-      this.addVariantToCart = this.addVariantToCart.bind(this);
-      this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
-      this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
-    }
-
-      componentWillMount() {
-        this.props.client.checkout.create().then((res) => {
-          this.setState({
-            checkout: res,
-          });
-        });
-
-        this.props.client.product.fetchAll().then((res) => {
-          this.setState({
-            products: res,
-          });
-        });
-
-        this.props.client.shop.fetchInfo().then((res) => {
-          this.setState({
-            shop: res,
-          });
-        });
-      }
-
-      addVariantToCart(variantId, quantity){
-        this.setState({
-          isCartOpen: true,
-        });
-
-        const lineItemsToAdd = [{variantId, quantity: parseInt(quantity, 10)}]
-        const checkoutId = this.state.checkout.id
-
-        return this.props.client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => {
-          this.setState({
-            checkout: res,
-          });
-        });
-      }
-
-      updateQuantityInCart(lineItemId, quantity) {
-        const checkoutId = this.state.checkout.id
-        const lineItemsToUpdate = [{id: lineItemId, quantity: parseInt(quantity, 10)}]
-
-        return this.props.client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => {
-          this.setState({
-            checkout: res,
-          });
-        });
-      }
-
-      removeLineItemInCart(lineItemId) {
-        const checkoutId = this.state.checkout.id
-
-        return this.props.client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => {
-          this.setState({
-            checkout: res,
-          });
-        });
-      }
-
-      handleCartClose() {
-        this.setState({
-          isCartOpen: false,
-        });
-      }
-
     render() {
       return (
         <div>
           <header className="header">
-            {!this.state.isCartOpen &&
-              <div className="App__view-cart-wrapper">
-                <button className="App__view-cart" onClick={()=> this.setState({isCartOpen: true})}>Basket</button>
-              </div>
-            }
             <NavLink to="/">
               <img className="header__logo" src={Image} alt="Wolf Rayet Logo"/>
             </NavLink>
@@ -101,14 +17,6 @@ class Header extends Component {
               <NavLink to="/contact">Contact</NavLink>
             </nav>
           </header>
-
-          <Cart
-            checkout={this.state.checkout}
-            isCartOpen={this.state.isCartOpen}
-            handleCartClose={this.handleCartClose}
-            updateQuantityInCart={this.updateQuantityInCart}
-            removeLineItemInCart={this.removeLineItemInCart}
-          />
         </div>
       )
     }
