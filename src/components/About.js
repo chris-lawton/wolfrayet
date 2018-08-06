72358baa7d80fce8ffa3626c26321dc37fd0ed41
Chris Lawton <chris.lawton@torchbox.com>
2018-08-06 20:54:34 +0100

    update about copy

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,11 @@ class About extends Component {
           <img className="about__image" src={Image} alt="Laura"/>
           <div>
             <h2>BACKGROUND</h2>
-            <p>Wolf Rayet is a British jewellery and fashion brand established in 2013. The jewellery is laser cut and crafted in London, All the jewellery is made from stainless steel and sterling silver chains. The name of the brand is taken from a famous dying star, bigger and brighter than the sun. Wolf Rayet as a brand is about making a bold statement, not afraid to stand out from the crowd. The pieces are designed to be an accent, transforming a whole outfit to make you feel and look special.</p>
+            <p>Wolf Rayet is a British fashion brand established in 2013.</p>
+
+            <p>All clothing and fabric is made in London, prints are all designed by Wolf Rayet. The name of the brand is taken from a famous dying star, that burns bigger and brighter than the sun. Wolf Rayet as a brand is about making a bold statement, not afraid to stand out from the crowd.</p>
+
+            <p>The catsuits are designed to transform anyone into a glamorous beauty to make you feel and look so special.</p>
 
             <h2>SHIPPING</h2>
             <p>Orders will normally be dispatched within 48 hours, during busy periods dispatch may take up to ten days.</p>
